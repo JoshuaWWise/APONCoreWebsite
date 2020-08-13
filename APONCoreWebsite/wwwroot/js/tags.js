b22b0cd918370c46c424fa33c66ead3834fbf9cf
Joshua Wise <JoshuaWWise78@gmail.com>
2020-08-13 07:40:40 -0400

    Finished the Tag Console

--- APONCoreWebsite/wwwroot/js/tags.js
+++ APONCoreWebsite/wwwroot/js/tags.js
@@ -23,17 +23,17 @@ function filterTagList() {
      
         var selectOption = document.createElement("OPTION");
       
-        var nameFieldElement = document.getElementById("Tag[" + i + "].name");
-        console.log(nameFieldElement.value);
+        var nameFieldElement = document.getElementById("Tag[" + i + "].Name");
+      
     
         selectOption.text = nameFieldElement.value;
   
         if (filterText == "" || selectOption.text.toLowerCase().includes(filterText.toLowerCase())) {
-            console.log("Tag[" + i + "].id");
-            var idField = document.getElementById("Tag[" + i + "].id");
+        
+            var idField = document.getElementById("Tag[" + i + "].Index");
             var idFieldValue = idField.value;
 
-            selectOption.value = idFieldValue;
+            selectOption.value = i;
             selector.appendChild(selectOption);
 
         }
@@ -43,42 +43,47 @@ function filterTagList() {
 
 function AddTagToList(sender) {
     var value = sender.value;
+ 
     var selectedTags = document.getElementById("selectedTagIndicies").value;
-
+    var selectedTagElement = document.getElementById("Tag[" + value + "].Index");
     var TagList = selectedTags.split(',');
-
+  
 
 
     if (!TagList.includes(value)) {
         selectedTags += "," + value;
+        document.getElementById("selectedTagIDs").value += "," + selectedTagElement.value;
         document.getElementById("selectedTagIndicies").value = selectedTags;
     }
-
+  
     populateSelectedTagList();
 
 
 }
 
 function populateSelectedTagList() {
+   
     var selectedTags = document.getElementById("selectedTagIndicies").value;
     var TagList = selectedTags.split(",");
     var ListItem = document.getElementById("tagList");
 
     ListItem.innerHTML = '';
-
+ 
     if (TagList.length > 0) {
 
         for (var i = 0; i < TagList.length; i++) {
+
            
             if (TagList[i] != "") {
                 var li = document.createElement("li");
-                li.setAttribute("id", ("tagListItemID--" + TagList[i]));
+                li.setAttribute("id", ("tagListIndex--" + TagList[i]));
                 li.addEventListener("click", removeSelectedTag);
                 li.classList.add("APONbtnBlue");
                 li.classList.add("APONbtn");
-              
-                var TagName = document.getElementsByName("TagID_" + TagList[i] + "_Name")[0].value;
-                //var TagName = document.getElementById('Tag[' + TagList[i] + '].name').value;
+                
+                var TagName = document.getElementById("Tag[" + TagList[i] + "].Name").value;
+                
+         
                 li.appendChild(document.createTextNode(TagName));
                 ListItem.appendChild(li);
             }
@@ -88,8 +93,19 @@ function populateSelectedTagList() {
 
 function removeSelectedTag(item) {
     var index = item.srcElement.id.split('--')[1];
+    var ID = document.getElementById("Tag[" + index + "].Index").value;
     var selectedTags = document.getElementById("selectedTagIndicies").value;
- 
+    var selectedIDS = document.getElementById("selectedTagIDs").value;
+    var newSelectedIDS = selectedIDS.replace(ID, "");
+    if (newSelectedIDS.substring(0, 1) == ",") {
+        if (newSelectedIDS.length > 1) {
+            newSelectedIDS = newSelectedIDS.substring(1, newSelectedIDS.length);
+        }
+        else {
+            newSelectedIDS = "";
+        }
+    }
+    document.getElementById("selectedTagIDs").value = newSelectedIDS;
     var newSelectedTags = selectedTags.replace((index + ","), "");
     newSelectedTags = newSelectedTags.replace(index, "");
     if (newSelectedTags.substring(0, 1) == ",") {
@@ -109,8 +125,12 @@ function removeSelectedTag(item) {
 
 
 function addNewTag() {
-   
+    //Store selected tag ID's before Ajax, then find them again after
+    
+
+
     var value = document.getElementById("txtTagFilter").value;
+  
     $.ajax({
         type: "POST",
     
@@ -127,31 +147,49 @@ function addNewTag() {
             containerElement.innerHTML = "";
 
             document.getElementById("TagModelCount").value = resp.length;
+            document.getElementById("selectedTagIndicies").value = "";
+            var selectedIndicies = "";
+            var selectedIDS = document.getElementById("selectedTagIDs").value;
 
-
+            var IDArray = selectedIDS.split(",");
+            console.log(IDArray);
             for (var j = 0; j < resp.length; j++) {
                 var newEl = document.createElement("input");
               
                
-               newEl.setAttribute("id", "Tag[" + j + "].id");
+               newEl.setAttribute("id", "Tag[" + j + "].Index");
               
                // newEl.id = id;
                 newEl.type = "hidden";
                 newEl.value = resp[j].tagID;
-                newEl.name = "'TagID_" + resp[j].tagID + "_'";
+
+                if (IDArray.includes(resp[j].tagID.toString())) {
+                    if (selectedIndicies.length == 0) {
+                        selectedIndicies = j;
+                        console.log(j);
+                    }
+                    else {
+                        selectedIndicies += "," + j;
+                        console.log(j);
+                    }
+                }
 
                 containerElement.appendChild(newEl);
 
                 newEl = document.createElement("input");
                 newEl.type = "hidden";
-                var nameTagID = "Tag[" + j + "].name";
+                var nameTagID = "Tag[" + j + "].Name";
                 newEl.id = nameTagID;
-                newEl.name = "'TagID_" + resp[j].tagID + "_Name'";
+                
                 newEl.value = resp[j].name;
                 containerElement.appendChild(newEl);
            
             }
-          
+
+           
+
+            document.getElementById("selectedTagIndicies").value = selectedIndicies;
+
             document.getElementById("txtTagFilter").value = "";
             filterTagList();
         }
